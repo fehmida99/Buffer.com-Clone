eaf4d203ce6f3b70488ffc56d9203f19bf49c181
Uzair <uzairmd073@gmail.com>
2022-12-19 13:07:59 +0530

    chnages

--- script/start.js
+++ script/start.js
@@ -2,77 +2,77 @@
 
 //mini database
 
-// let arr2 =[
-//     {
-//         "title":"Traveler",
-//         "img":"https://buffer-start-page.s3.amazonaws.com/presets/Default_X2.png"
+ let arr2 =[
+    {
+        "title":"Traveler",
+        "img":"https://buffer-start-page.s3.amazonaws.com/presets/Default_X2.png"
         
-//     },
-//     {
-//         "title":"Flower shop",
-//         "img":"https://buffer-start-page.s3.amazonaws.com/presets/Flower_X2.png"
+    },
+    {
+        "title":"Flower shop",
+        "img":"https://buffer-start-page.s3.amazonaws.com/presets/Flower_X2.png"
         
-//     },
-//     {
-//         "title":"Pizza",
-//         "img":"https://buffer-start-page.s3.amazonaws.com/presets/Pizza_X2.png"
+    },
+    {
+        "title":"Pizza",
+        "img":"https://buffer-start-page.s3.amazonaws.com/presets/Pizza_X2.png"
         
-//     },
-//     {
-//         "title":"Alexoo",
-//         "img":"https://buffer-start-page.s3.amazonaws.com/presets/Alexoo_X2.png"
+    },
+    {
+        "title":"Alexoo",
+        "img":"https://buffer-start-page.s3.amazonaws.com/presets/Alexoo_X2.png"
         
-//     },
-//     {
-//         "title":"Alba",
-//         "img":"https://buffer-start-page.s3.amazonaws.com/presets/Alba_X2.png"
+    },
+    {
+        "title":"Alba",
+        "img":"https://buffer-start-page.s3.amazonaws.com/presets/Alba_X2.png"
         
-//     },
-//     {
-//         "title":"Novika",
-//         "img":"https://buffer-start-page.s3.amazonaws.com/presets/Novika_X2.png"
+    },
+    {
+        "title":"Novika",
+        "img":"https://buffer-start-page.s3.amazonaws.com/presets/Novika_X2.png"
         
-//     },
-//     {
-//         "title":"Scot See",
-//         "img":"https://buffer-start-page.s3.amazonaws.com/presets/Scott_X2.png"
+    },
+    {
+        "title":"Scot See",
+        "img":"https://buffer-start-page.s3.amazonaws.com/presets/Scott_X2.png"
         
-//     },
-//     {
-//         "title":"Laura and James",
-//         "img":"https://buffer-start-page.s3.amazonaws.com/presets/Laurajames_X2.png"
+    },
+    {
+        "title":"Laura and James",
+        "img":"https://buffer-start-page.s3.amazonaws.com/presets/Laurajames_X2.png"
         
-//     },
-//     {
-//         "title":"Simone",
-//         "img":"https://buffer-start-page.s3.amazonaws.com/presets/Simone_X2.png"
+    },
+    {
+        "title":"Simone",
+        "img":"https://buffer-start-page.s3.amazonaws.com/presets/Simone_X2.png"
         
-//     },
-//     {
-//         "title":"Wang",
-//         "img":"https://buffer-start-page.s3.amazonaws.com/presets/Wang_X2.png"
+    },
+    {
+        "title":"Wang",
+        "img":"https://buffer-start-page.s3.amazonaws.com/presets/Wang_X2.png"
         
-//     },
-//     {
-//         "title":"Emmenual",
-//         "img":"https://buffer-start-page.s3.amazonaws.com/presets/emmanuel_X2.png"
+    },
+    {
+        "title":"Emmenual",
+        "img":"https://buffer-start-page.s3.amazonaws.com/presets/emmanuel_X2.png"
         
-//     },
-//     {
-//         "title":"Mogli",
-//         "img":"https://buffer-start-page.s3.amazonaws.com/presets/mogli_X2.png"
+    },
+    {
+        "title":"Mogli",
+        "img":"https://buffer-start-page.s3.amazonaws.com/presets/mogli_X2.png"
         
-//     },
-//     {
-//         "title":"Sculpture NFT",
-//         "img":"https://buffer-start-page.s3.amazonaws.com/presets/SculptureNFT_X2.gif"
+    },
+    {
+        "title":"Sculpture NFT",
+        "img":"https://buffer-start-page.s3.amazonaws.com/presets/SculptureNFT_X2.gif"
         
-//     },
+    },
     
 
-// ]
+]
 
-let arr = JSON.parse(localStorage.getItem("newarr"))
+let arr = JSON.parse(localStorage.getItem("newarr")) || arr2
 
 
 
